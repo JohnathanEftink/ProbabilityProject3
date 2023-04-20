25413088c00a4138a58f333d566a330b8c2803ad
Maxwell Penders <max.penders@gmail.com>
2023-04-20 00:32:11 -0400

    Have output data going to file now for graphing

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Main {
     public static void main(String args[]){
-	
+	File outFile = new File("simdata");
 	Simulation sim;
+	try {
+	FileWriter writer = new FileWriter("simdata");
 	sim = new Simulation(10);
-	System.out.println(sim.runSimulation());
+	writer.write(String.valueOf(sim.runSimulation()) + '\n');
 	for (int i = 0; i < 110; i++) {
 		sim.resetSimulation(10);
-		System.out.println(sim.runSimulation());
+		writer.write(String.valueOf(sim.runSimulation()) + '\n');
 	}
-
-    }
+	writer.close();
+	} catch (Exception e) {
+		System.out.println("Problem with larger loop: " + e);
+	}
+}
 }
