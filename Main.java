4e23a8f394314a209d5a2fa51f9464e6277edc29
Maxwell Penders <max.penders@gmail.com>
2023-04-20 15:27:26 -0400

    Formatted file

--- Main.java
+++ Main.java
@@ -2,20 +2,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 public class Main {
-    public static void main(String args[]){
-	File outFile = new File("simdata");
-	Simulation sim;
-	try {
-	FileWriter writer = new FileWriter("simdata");
-	sim = new Simulation(10);
-	writer.write(String.valueOf(sim.runSimulation()) + '\n');
-	for (int i = 0; i < 110; i++) {
-		sim.resetSimulation(10);
-		writer.write(String.valueOf(sim.runSimulation()) + '\n');
+	public static void main(String args[]){
+		File outFile = new File("simdata");
+		Simulation sim;
+		try {
+			FileWriter writer = new FileWriter("simdata");
+			sim = new Simulation(10);
+			writer.write(String.valueOf(sim.runSimulation()) + '\n');
+			for (int i = 0; i < 110; i++) {
+				sim.resetSimulation(10);
+				writer.write(String.valueOf(sim.runSimulation()) + '\n');
+			}
+			writer.close();
+		} catch (Exception e) {
+			System.out.println("Problem with larger loop: " + e);
+		}
 	}
-	writer.close();
-	} catch (Exception e) {
-		System.out.println("Problem with larger loop: " + e);
-	}
-}
 }
